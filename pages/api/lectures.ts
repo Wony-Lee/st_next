ce442f37c6c0291d312340a1b4c45ce15169469c
Wony-Lee <a1_11_11@naver.com>
2021-12-23 23:26:13 +0900

    mockData Update

---
+++ pages/api/lectures.ts
@@ -0,0 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const mockCategory = {
+    id: 0,
+    name: "프로그래밍",
+};
+const tags = [
+    {
+        id: 0,
+        name: "개발",
+    },
+    {
+        id: 1,
+        name: "AWS",
+    },
+    {
+        id: 2,
+        name: "DevOps",
+    },
+];
+const lectureData = {
+    lectureList: [
+        {
+            id: 0,
+            category: mockCategory,
+            title: "개발/운영/아키텍트를 아우르는 AWS/DevOps",
+            tags: tags,
+            description: "개발/운영/아키텍트를 아우르는 AWS/DevOps",
+            thumb: "/thumb.jpg",
+            isHot: true,
+            isNew: true,
+        },
+    ],
+};
+
+type Data = {
+    name: string;
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).json(lectureData);
+}
